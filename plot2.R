5a604f5980bac4001c30eccd614af260a90cf61b
zmodda <zmodda@gmail.com>
2014-05-25 16:02:24 -0500

    Plot that makes plot2.png

--- plot2.R
+++ plot2.R
@@ -14,6 +14,8 @@
 
 ## Set the directory where the household power consumption data is. 
 setwd("C:\\Users\\adriana dominguez\\Documents\\CourseraDataScience\\EDA")
+#setwd("C:\\workhere\\EDA")
+#C:\workhere\EDA
 
 #### Get the datasets needed from the folder where you downloaded everything.
 SourceClassCode <- readRDS(".\\Source_Classification_Code.rds")
@@ -24,42 +26,16 @@ sourceClass <- SourceClassCode
 summaryPM <- summarySCCPM25
 
 #####################################################################################
-## Question 1                                                                      ##
-##################################################################################### 
-
-## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-## Using the base plotting system, make a plot showing the total PM2.5 emission from 
-## all sources for each of the years 1999, 2002, 2005, and 2008.
-
-data1999 <- summaryPM[summaryPM$year==1999,]
-dim(data1999)
-s1 <- sum(data1999$Emissions)
-
-data2002 <- summaryPM[summaryPM$year==2002,]
-dim(data2002)
-s2 <- sum(data2002$Emissions)
-
-data2005 <- summaryPM[summaryPM$year==2005,]
-dim(data2005)
-s3 <- sum(data2005$Emissions)
-
-data2008 <- summaryPM[summaryPM$year==2008,]
-dim(data2008)
-s4 <- sum(data2008$Emissions)
-
-
-## We can see from this simple plot that the total emissions have decreased over the years.
-plot(c(1999,2002,2005,2008),c(s1,s2,s3,s4),col="red",pch=16,main="Total Emissions",xlab="Year",ylab="Total Emissions")
-dev.copy(png, file = "plot1.png")
-dev.off()
-
+## Question 2                                                                      ##
+#####################################################################################
 ## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland 
 ## (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot 
 ## answering this question.
 
-baltimoreData <- summaryPM[summaryPM$fips=="24510"]
+## summaryPM will contain the Baltimore data
+summaryPM <- summaryPM[summaryPM$fips=="24510",]
 
-data1999 <- baltimoreData[baltimoreData$year==1999,]
+data1999 <- summaryPM[summaryPM$year==1999,]
 s1 <- sum(data1999$Emissions)
 
 data2000 <- summaryPM[summaryPM$year==2000,]
@@ -75,7 +51,7 @@ data2003 <- summaryPM[summaryPM$year==2003,]
 s5 <- sum(data2003$Emissions)
 
 data2004 <- summaryPM[summaryPM$year==2004,]
-s6 <- sum(data2003$Emissions)
+s6 <- sum(data2004$Emissions)
 
 data2005 <- summaryPM[summaryPM$year==2005,]
 s7 <- sum(data2005$Emissions)
@@ -83,38 +59,15 @@ s7 <- sum(data2005$Emissions)
 data2006 <- summaryPM[summaryPM$year==2006,]
 s8 <- sum(data2006$Emissions)
 
-data2007 <- summaryPM[summaryPM$year==2006,]
+data2007 <- summaryPM[summaryPM$year==2007,]
 s9 <- sum(data2007$Emissions)
 
-
-data2002 <- summaryPM[summaryPM$year==2002,]
-dim(data2002)
-s2 <- sum(data2002$Emissions)
-
-data2005 <- summaryPM[summaryPM$year==2005,]
-dim(data2005)
-s3 <- sum(data2005$Emissions)
-
 data2008 <- summaryPM[summaryPM$year==2008,]
-dim(data2008)
-s4 <- sum(data2008$Emissions)
+s10 <- sum(data2008$Emissions)
 
+yearData <- c(1999,2000,2001,2002,2003,2004,2005,2006,2007,2008)
+totalEmission <- c(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10)
 
-plot(c(1999,2002,2005,2008),c(s1,s2,s3,s4),col="red",pch=16,main="Total Emissions",xlab="Year",ylab="Total Emissions")
-dev.copy(png, file = "plot1.png")
+plot(yearData,totalEmission,col="darkorchid4",pch=15,main="Total Emissions in Baltimore",xlab="Year",ylab="Total Emissions")
+dev.copy(png, file = "plot2.png")
 dev.off()
-
-
-## Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) 
-## variable, which of these four sources have seen decreases in emissions from 1999–2008 
-## for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the 
-## ggplot2 plotting system to make a plot answer this question.
-
-## Across the United States, how have emissions from coal combustion-related sources 
-## changed from 1999–2008?
-
-## How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore 
-## City?
-
-## Compare emissions from motor vehicle sources in Baltimore City with emissions from 
-## motor vehicle sources in Los Angeles County, California (fips == "06037"). Which city has seen greater changes over time in motor vehicle emissions?
